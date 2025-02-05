77ae778fcd5ba74fb633214918a515d448b1c2b1
John Zhou <johnz.webdesign@gmail.com>
2025-02-05 16:54:05 -0500

    set up App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Link } from "react-router-dom";
+import SubjectCard from "../components/SubjectCard";
 
-function App() {
-  const [count, setCount] = useState(0)
+const subjects = [
+  { name: "JavaScript", path: "/notes/javascript" },
+  { name: "React", path: "/notes/react" },
+  { name: "CSS", path: "/notes/css" },
+];
 
+const Home = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1>Welcome to jNote</h1>
+      <div className="subjects-container">
+        {subjects.map((subject, index) => (
+          <Link key={index} to={subject.path}>
+            <SubjectCard title={subject.name} />
+          </Link>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default Home;
