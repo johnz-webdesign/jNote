0cc6931da9c3505157bd9f66c0a315acca9e01f1
John Zhou <johnz.webdesign@gmail.com>
2025-02-06 18:10:41 -0500

    homepage styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,19 +6,21 @@ const Home = () => {
   return (
     <div
       data-t="wrapper"
-      className="center min-h-screen flex-col gap-5 rounded-lg bg-bg"
+      className="center min-h-screen flex-col gap-7 rounded-lg bg-bg"
     >
-      <div className="flex flex-col gap-4">
-        <h1 className="text-5xl font-bold">Welcome to jNote</h1>
-        <h2>Write down your thoughts, worries, hopes and dreams</h2>
-        <hr className="border border-p" />
+      <div className="center flex-col gap-3.5">
+        <h1 className="text-6xl text-blue">Welcome to jNote</h1>
+        <h2 className="mb-2.5">
+          Write down your thoughts, worries, hopes and dreams
+        </h2>
+        <hr className="w-full border border-p" />
       </div>
       <div className="grid grid-cols-4 gap-7">
         {subjects.map((subject) => (
           <p key={subject}>
             <Link
               to={`/notes/${subject.toLowerCase()}`}
-              className="bg-blue-500 hover:bg-blue-600 block rounded-lg px-6 py-3 text-center text-white shadow transition"
+              className="center hover:brightness-120 rounded-lg bg-card p-5 text-lg text-h shadow-[0_0_8px_3px_#A9F2FF] transition-all duration-300 hover:-translate-y-2 hover:opacity-75"
             >
               {subject}
             </Link>
